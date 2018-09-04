dda7db4e41cb02cf235ea5e751572dc61cce24c7
Gabriel Duquette <gabrielduquette@gmail.com>
2018-09-04 16:54:49 -0400

    first stab at recipe_batches.py

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,11 +3,19 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass
+    counts = {}
+    for key, value in recipe.items():
+        if not key in ingredients:
+            return 0
+        elif key in counts:
+            counts[key] += ingredients[key] // recipe[key]
+        else:
+            counts[key] = ingredients[key] // recipe[key]
+    return min(counts.values())
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
+  # Change the entries of these dictionaries to test
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
