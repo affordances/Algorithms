a7fcd6bf9ce26fb0eb76b88874afdb7f4f0c97be
Gabriel Duquette <gabrielduquette@gmail.com>
2018-09-04 00:19:38 -0400

    stock_prices.py in linear time

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,33 @@
 
 import argparse
 
+# def find_max_profit(prices):
+#   highest = None
+#   for i, first_num in enumerate(prices):
+#       for second_num in prices[i + 1:]:
+#           current_profit = second_num - first_num
+#           if highest == None or current_profit > highest:
+#               highest = current_profit
+#   return highest
+
+# def find_max_profit(prices):
+#   highest = None
+#   for i, num in enumerate(prices[:-1]):
+#       high = max(prices[i + 1:])
+#       current_profit = high - num
+#       if highest == None or current_profit > highest:
+#           highest = current_profit
+#   return highest
+
 def find_max_profit(prices):
-  highest = None
-  for i, first_num in enumerate(prices):
-      for second_num in prices[i + 1:]:
-          current_profit = second_num - first_num
-          if highest == None or current_profit > highest:
-              highest = current_profit
-  return highest
+  lowest = prices[0]
+  profit = None
+  for num in prices[1:]:
+      if profit == None or num - lowest > profit:
+          profit = num - lowest
+      if num < lowest:
+          lowest = num
+  return profit
 
 
 if __name__ == '__main__':
