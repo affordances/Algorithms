dcdffa10716c7b9da8558d18f1a9641818383b0b
Gabriel Duquette <gabrielduquette@gmail.com>
2018-09-03 22:23:54 -0400

    first stab at stock_prices.py

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,11 +3,17 @@
 import argparse
 
 def find_max_profit(prices):
-  pass 
+  highest = None
+  for i, first_num in enumerate(prices):
+      for second_num in prices[i + 1:]:
+          current_profit = second_num - first_num
+          if highest == None or current_profit > highest:
+              highest = current_profit
+  return highest
 
 
 if __name__ == '__main__':
-  # You can test your implementation by running 
+  # You can test your implementation by running
   # `python stock_prices.py [prices]` where prices is comprised of
   # space-separated integer values
   parser = argparse.ArgumentParser(description='Find max profit from prices.')
